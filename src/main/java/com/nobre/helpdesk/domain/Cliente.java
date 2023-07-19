e766e6a7b1da6aa9a2fb5472523a68910fc5cb40
Nobr3c <jnobr3c+github@gmail.com>
2023-07-19 20:56:24 -0300

    :books: Aula 07 - Classes de Domínio (Parte 02) - Anotações JPA para Classes de Domínio

--- src/main/java/com/nobre/helpdesk/domain/Cliente.java
+++ src/main/java/com/nobre/helpdesk/domain/Cliente.java
@@ -5,21 +5,25 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.nobre.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa{
 	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
