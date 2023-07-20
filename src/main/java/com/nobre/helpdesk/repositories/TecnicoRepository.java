b506a7f08167f5bf2d6d5e08738359f68997e497
Nobr3c <jnobr3c+github@gmail.com>
2023-07-20 01:22:47 -0300

    :books: Aula 07 - Carga Inicial da Base de Dados (final)

---
+++ src/main/java/com/nobre/helpdesk/repositories/TecnicoRepository.java
@@ -0,0 +1,9 @@
+package com.nobre.helpdesk.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nobre.helpdesk.domain.Tecnico;
+
+public interface TecnicoRepository extends JpaRepository<Tecnico, Integer>{
+
+}
