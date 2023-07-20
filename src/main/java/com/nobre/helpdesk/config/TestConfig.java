f18b3881f8e6e23f9fdb68dee7f4bab7be9de699
Nobr3c <jnobr3c+github@gmail.com>
2023-07-20 20:44:27 -0300

    :books: Aula 08 - Perfil de Teste (Criação)

--- src/main/java/com/nobre/helpdesk/config/TestConfig.java
+++ src/main/java/com/nobre/helpdesk/config/TestConfig.java
@@ -1,10 +1,21 @@
 package com.nobre.helpdesk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.nobre.helpdesk.services.DBServices;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 
+	@Autowired
+	private DBServices dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
 }
