344ef0c514184e92b62f345115b03b5247286b0b
Nobr3c <jnobr3c+github@gmail.com>
2023-09-26 22:17:29 -0300

    :books: Exceção para objeto não enontrado

--- src/main/java/com/nobre/helpdesk/services/TecnicoService.java
+++ src/main/java/com/nobre/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nobre.helpdesk.domain.Tecnico;
 import com.nobre.helpdesk.repositories.TecnicoRepository;
+import com.nobre.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto Não Encontrado: " + id));
 	}
 }
