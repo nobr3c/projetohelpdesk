0f3598a724acfa94a20df200038a02e225e2f418
Nobr3c <jnobr3c+github@gmail.com>
2023-09-27 07:21:38 -0300

    :books: Endpoint create para Tecnico

--- src/main/java/com/nobre/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/nobre/helpdesk/resources/TecnicoResource.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nobre.helpdesk.domain.Tecnico;
 import com.nobre.helpdesk.domain.dtos.TecnicoDTO;
 import com.nobre.helpdesk.services.TecnicoService;
-import java.util.*;
-import java.util.stream.Collector;
+
+import java.net.URI;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
@@ -35,5 +39,12 @@ public class TecnicoResource {
 		List<Tecnico> list = service.findAll();
 		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO){
+		Tecnico newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/nobre/helpdesk/services/TecnicoService.java
+++ src/main/java/com/nobre/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nobre.helpdesk.domain.Tecnico;
+import com.nobre.helpdesk.domain.dtos.TecnicoDTO;
 import com.nobre.helpdesk.repositories.TecnicoRepository;
 import com.nobre.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 	
 	public List<Tecnico> findAll(){
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
