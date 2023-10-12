46d9488160fc10d2335a73dd748c843ad6211021
Nobr3c <jnobr3c+github@gmail.com>
2023-10-12 12:39:47 -0300

    :books: Aula 16 Validations

--- src/main/java/com/nobre/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/nobre/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nobre.helpdesk.domain.Tecnico;
 import com.nobre.helpdesk.domain.enums.Perfil;
@@ -14,9 +16,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
+	@NotNull(message = "O Campo NOME é requerido")
 	protected String nome;
+	@NotNull(message = "O Campo CPF é requerido")
 	protected String cpf;	
+	@NotNull(message = "O Campo E-MAIL é requerido")
 	protected String email;
+	@NotNull(message = "O Campo SENHA é requerido")
 	protected String senha;	
 	protected Set<Integer> perfis = new HashSet<>();
 	
